6cb0e18598e7c8cee70046a60970ca439d8c639c
John S. Ryan <jtigger@infosysengr.com>
2018-02-14 05:43:50 -0800

    Inception.

---
+++ wwwrps/src/main/java/io/pivotal/xpppp/wwwrps/WwwRpsApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.xpppp.wwwrps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwRpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwwRpsApplication.class, args);
+	}
+}
---
+++ wwwrps/src/test/java/io/pivotal/xpppp/wwwrps/WwwRpsApplicationTests.java
@@ -0,0 +1,16 @@
+package io.pivotal.xpppp.wwwrps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WwwRpsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
